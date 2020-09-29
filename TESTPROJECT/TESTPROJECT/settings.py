f239b080b2551179c8f69450cb1bc3f5ea617f08
Akash218 <71888336+Akash218@users.noreply.github.com>
2020-09-29 17:38:55 +0530

    Update settings.py

--- TESTPROJECT/TESTPROJECT/settings.py
+++ TESTPROJECT/TESTPROJECT/settings.py
@@ -134,5 +134,5 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER='16EC004@kcgcollege.com'
-EMAIL_HOST_PASSWORD='nallathambi'
+EMAIL_HOST_USER='xyz123@gmail.com'
+EMAIL_HOST_PASSWORD='abcdefgh'
